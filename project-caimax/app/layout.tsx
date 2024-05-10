b265fbd8dc4f59e6381c33fe7c721e713cf5434c
MauriBoye <mauriboye@gmail.com>
2024-05-10 20:43:27 -0300

    Parametros opengraph

--- project-caimax/app/layout.tsx
+++ project-caimax/app/layout.tsx
@@ -7,6 +7,7 @@ import { ThemeProvider } from '@/context/theme-provider';
 import { NextUIProviderContext } from '@/context/nextui-provider';
 
 export const metadata: Metadata = {
+  metadataBase: new URL('https://caimax.vercel.app/'),
   title: {
     template: '%s | CAIMAX Dashboard',
     default: 'CAIMAX Dashboard',
@@ -17,15 +18,17 @@ export const metadata: Metadata = {
     title: 'CAIMAX Dashboard',
     description:
       'Proyecto CAIMAX de la materia Laboratorio de Sistemas Embebidos UNRN',
+    url: 'https://caimax.vercel.app/',
+    siteName: 'CAIMAX',
+    images: [
+      {
+        url: './opengraph-image.png', // Must be an absolute URL
+        width: 800,
+        height: 600,
+      },
+    ],
+    locale: 'en_US',
     type: 'website',
-    locale: 'es_AR',
-    url: 'https://caimax.vercel.app',
-    images: {
-      url: './opengraph-image.png',
-      width: 800,
-      height: 600,
-      alt: 'CAIMAX OpenGraph Image',
-    },
   },
 };
 export default function RootLayout({
